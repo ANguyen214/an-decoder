91e3df02adb74421dde0bd2dc22dba5e2f7d68ff
Alexander Nguyen <41305327+nguyenalex214@users.noreply.github.com>
2021-05-18 23:46:10 -0700

    substitution.js is complete and all tests passed.

--- src/substitution.js
+++ src/substitution.js
@@ -5,15 +5,60 @@
 
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
+  let alphaArray = ['a','b','c','d','e',
+                    'f','g','h','i','j',
+                    'k','l','m','n','o',
+                    'p','q','r','s','t',
+                    'u','v','w','x','y','z'];
+  function dupliCheck(alphabet)
+  {
+    let setMethod = new Set(alphabet);
+    return(setMethod.size === 26);
+  }
 
   function substitution(input, alphabet, encode = true) {
     // your solution code here
-    
+    let cipher = new Object();
+    let codeString = "";
+
+    if(!dupliCheck(alphabet))
+      {
+        return false;
+      }
+
+    for(let i = 0; i < alphabet.length; i++)
+      {
+        if(encode)
+          {
+            cipher[alphaArray[i]] = alphabet[i];
+          }
+        else
+          {
+            cipher[alphabet[i]] = alphaArray[i];
+          }
+      }
+
+    const cipherKeys = Object.keys(cipher);
+
+    for(let i = 0; i < input.length; i++)
+      {
+        if(input[i] == " ")
+          {
+            codeString += input[i];
+          }
+        else
+          {
+            findKey = cipherKeys.find((keyVar) => keyVar === input[i]);
+            codeString += cipher[findKey];
+          }
+      }
+    return codeString;
   }
 
   return {
-    substitution,
+    substitution, dupliCheck,
   };
 })();
+
 
 module.exports = { substitution: substitutionModule.substitution };
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,40 @@
 // Write your tests here!
+const expect = require("chai").expect;
+const {substitution} = require("../src/substitution");
+
+describe("substitution", () => {
+    it("Should take in a string and encode it: thinkful => jrufscpw", () => {
+        const input = "thinkful";
+        const expected = "jrufscpw";
+        const actual = substitution(input, "xoyqmcgrukswaflnthdjpzibev");
+        expect(actual).to.equal(expected);
+    });
+
+    it("Testing with special characters: message => y&ii$r&", () => {
+        const input = "message";
+        const expected = "y&ii$r&";
+        const actual = substitution(input, "$wae&zrdxtfcygvuhbijnokmpl");
+        expect(actual).to.equal(expected);
+    });
+
+    it("Testing with special characters: y&ii$r& => message", () => {
+        const input = "y&ii$r&";
+        const expected = "message";
+        const actual = substitution(input, "$wae&zrdxtfcygvuhbijnokmpl", false);
+        expect(actual).to.equal(expected);
+    });
+
+    it("Testing for a false result with a short alphabet input", () => {
+        const input = "thinkful";
+        const expected = false;
+        const actual = substitution(input, "short");
+        expect(actual).to.equal(expected);
+    });
+
+    it("Testing for a false result with an alphabet input that's too long", () => {
+        const input = "thinkful";
+        const expected = false;
+        const actual = substitution(input, "short");
+        expect(actual).to.equal(expected);
+    });
+});
