0a74b69b91b455106394d14e02d6dab643ee7ebc
Alexander Nguyen <41305327+nguyenalex214@users.noreply.github.com>
2021-05-18 18:39:35 -0700

    polybius.js is completed and passed testing

--- src/polybius.js
+++ src/polybius.js
@@ -5,9 +5,56 @@
 
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
+  let polyDecode = {1: {1: 'a', 2: 'f', 3: 'l', 4: 'q', 5: 'v',},
+                    2: {1: 'b', 2: 'g', 3: 'm', 4: 'r', 5: 'w',},
+                    3: {1: 'c', 2: 'h', 3: 'n', 4: 's', 5: 'x',},
+                    4: {1: 'd', 2: '(i/j)', 3: 'o', 4: 't', 5: 'y',},
+                    5: {1: 'e', 2: 'k', 3: 'p', 4: 'u', 5: 'z',},};
+  
+  let polyEncode = { a: 11, b: 21, c: 31, d: 41, e: 51,   
+                     f: 12, g: 22, h: 32, i: 42, j: 42,
+                     k: 52, l: 13, m: 23, n: 33, o: 43, 
+                     p: 53, q: 14, r: 24, s: 34, t: 44, 
+                     u: 54, v: 15, w: 25, x: 35, y: 45, z: 55,} 
+                     
 
   function polybius(input, encode = true) {
     // your solution code here
+
+    let codeString = "";
+    for(i = 0; i < input.length; i++)
+      {
+        let asciiNum = input.toLowerCase().charCodeAt(i);
+        if(encode)
+          {
+            if(asciiNum >= 97 && asciiNum <= 122)
+              {
+                codeString += polyEncode[input[i].toLowerCase()];
+              }
+            else
+              {
+                codeString += input[i];
+              }
+          }
+        else
+          {
+            let noSpaceInput = input.replace(/ /g, "");
+            if(noSpaceInput.length % 2 !== 0)
+              {
+                return false;
+              }
+            else if(asciiNum >= 49 && asciiNum <= 57)
+              {
+                codeString += polyDecode[input[i]][input[i + 1]];
+                i++;
+              }
+            else
+              {
+                codeString += input[i];
+              }
+          }
+      }
+      return codeString;
   }
 
   return {
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,40 @@
 // Write your tests here!
+const expect = require("chai").expect;
+const {polybius} = require("../src/polybius");
+
+describe("polybius", () => {
+    it("Should encode the input and return string of numbers", () => {
+        const input = "thinkful";
+        const expected = "4432423352125413"
+        const actual = polybius(input);
+        expect(actual).to.equal(expected);
+    });
+
+    it("Should encode input regardless of capital letters and spaces", () => {
+        const input = "Hello world";
+        const expected = "3251131343 2543241341";
+        const actual = polybius(input);
+        expect(actual).to.equal(expected);
+    });
+
+    it("Should decode the input and return a string", () => {
+        const input = "3251131343 2543241341";
+        const expected = "hello world";
+        const actual = polybius(input, false);
+        expect(actual).to.equal(expected);
+    });
+
+    it("Should decode the input and return a string. Testing (i/j)", () => {
+        const input = "4432423352125413";
+        const expected = "th(i/j)nkful";
+        const actual = polybius(input, false);
+        expect(actual).to.equal(expected);
+    });
+
+    it("Should return false when input is a string of numbers and length is odd", () => {
+        const input = "44324233521254131";
+        const expected = false;
+        const actual = polybius(input, false);
+        expect(actual).to.equal(false);
+    })
+});
